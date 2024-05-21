7767b945054bdf9317445c222580c3aae9292ca3
Zubair Khan <159756145+zubairahmedkha@users.noreply.github.com>
2024-05-21 12:54:12 +0500

    work

---
+++ 21-5/main.js
@@ -0,0 +1,30 @@
+"use strict";
+//controllers flow if
+const userLoggedIn = true;
+const temrature = 40;
+if (temrature < 50) {
+    console.log("tempraure less than executeded user");
+}
+else {
+    console.log("tempraure is greater than 50");
+}
+const month = 1;
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    default: console.log("invalid month");
+}
+// false value false 0.-0,"",nullc,Nan,undefined
+//truethyh value true,"0"," v",[],{},function
+let val;
+// val =5 ?? 10
+// val =null ?? 10
+val = undefined !== null && undefined !== void 0 ? undefined : 10;
+console.log(val);
--- 21-5/main.ts
+++ 21-5/main.ts
@@ -1,2 +1,27 @@
-    //controllers flow
+    //controllers flow if
+    const userLoggedIn = true;
+    const temrature =40; 
+    if(temrature <50){
+console.log("tempraure less than executeded user");
+
+    }else{console.log("tempraure is greater than 50");}
+    const month :number= 1;
     
+    switch (month ) {
+        case 1:console.log("january");
+         break;
+    case 2:console.log("february");
+     break;
+     case 3:console.log("march");  
+            break;
+            default:console.log("invalid month");
+    }
+    // false value false 0.-0,"",nullc,Nan,undefined
+    //truethyh value true,"0"," v",[],{},function
+let val;
+// val =5 ?? 10
+// val =null ?? 10
+val =undefined ?? 10
+
+console.log(val);
+
